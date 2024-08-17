e241b336281868f894c6b8a9d51582000941a83f
suraj negi <negisuraj080@gmail.com>
2024-08-17 17:49:17 +0530

    third

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -24,14 +24,14 @@ const Input = () => {
 
 
    //Fetch data using .then
-//    const fetchDataThen = () => {
-//     fetch(
-//       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
-//     )
-//       .then((response) => response.json())
-//       .then((data) => setCripto(data))
-//       .catch((error) => console.error('Error fetching data:', error));
-//   };
+   const fetchDataThen = () => {
+    fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+    )
+      .then((response) => response.json())
+      .then((data) => setCripto(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  };
 
   const filteredCoins = cripto
     .filter((coin) => 
